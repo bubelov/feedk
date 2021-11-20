4749273360014016b3e7c832599af7d96aa66d1f
Igor Bubelov <igor@bubelov.com>
2021-11-20 17:14:31 +0700

    Allow re-using existing connections

--- src/main/kotlin/Feed.kt
+++ src/main/kotlin/Feed.kt
@@ -2,18 +2,20 @@ package co.appreactor.feedk
 
 import java.net.HttpURLConnection
 import java.net.URL
+import java.net.URLConnection
 import javax.xml.parsers.DocumentBuilderFactory
 
 sealed class Feed
 
-fun feed(url: URL): FeedResult {
+fun feed(url: URL, openedConnection: URLConnection? = null): FeedResult {
     val documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
 
-    val connection = runCatching {
-        url.openConnection()
-    }.getOrElse {
-        return FeedResult.HttpConnectionFailure(it)
-    }
+    val connection = openedConnection
+        ?: runCatching {
+            url.openConnection()
+        }.getOrElse {
+            return FeedResult.HttpConnectionFailure(it)
+        }
 
     runCatching {
         if (connection is HttpURLConnection) {
